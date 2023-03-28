40b7dca026f7d32b28d5692a0d9bfd3eff642d0b
Ajdin Imsirovic <ajdika@live.com>
2023-03-28 16:00:06 +0200

    004, Render a single square on the board

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Square from './Square';
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  console.log(squares);
+
+  function renderSquare(i) {
+    return (
+      <Square
+        value={squares[i] + 1}
+        onClick={() => {} }
+      />
+    );
+  }
+
   return (
     <div>
       <h1>Tic-Tac-Toe</h1>
       <div className="board">
-        The board squares will go here
+        {renderSquare(0)}
       </div>
     </div>
   );
