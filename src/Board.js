9b2584e0b4d55968c3453bfadc026d41c5cebd4f
Ajdin Imsirovic <ajdika@live.com>
2023-03-28 15:38:45 +0200

    003, Add and render Board.js

---
+++ src/Board.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Board() {
+  return (
+    <div>
+      <h1>Tic-Tac-Toe</h1>
+      <div className="board">
+        The board squares will go here
+      </div>
+    </div>
+  );
+}
+
+export default Board;
