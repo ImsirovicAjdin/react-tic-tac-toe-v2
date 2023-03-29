6d39f6fa5549ace4448ca3378fd5dd3fe59696ce
Ajdin Imsirovic <ajdika@live.com>
2023-03-29 04:11:05 +0200

    005, Add all the nine squares

--- src/Board.js
+++ src/Board.js
@@ -19,6 +19,14 @@ function Board() {
       <h1>Tic-Tac-Toe</h1>
       <div className="board">
         {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
